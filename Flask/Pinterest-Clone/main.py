97da638b1a3ed3e7048c1caf914858cd36b30799
DrkCde15 <jcesarsantana215@gmail.com>
2026-05-20 12:39:06 -0300

    feat: adiciona rotas e templates para a página inicial e perfil

--- Flask/Pinterest-Clone/main.py
+++ Flask/Pinterest-Clone/main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Pinterest Clone'
+    return render_template('index.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
