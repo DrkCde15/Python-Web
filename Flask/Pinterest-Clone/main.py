0774632bc5790bf654555f6cb772964285068034
DrkCde15 <jcesarsantana215@gmail.com>
2026-05-20 12:56:10 -0300

    feat: atualiza rota de perfil para aceitar parâmetro de usuário e modifica template para exibir informações do usuário

--- Flask/Pinterest-Clone/main.py
+++ Flask/Pinterest-Clone/main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/perfil')
-def perfil():
-    return render_template('perfil.html')
+@app.route('/perfil/<user>')
+def perfil(user):
+    return render_template('perfil.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
