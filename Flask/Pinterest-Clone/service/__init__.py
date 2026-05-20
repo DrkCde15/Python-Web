c541e994af5919538e91d1599e194d65642b81a9
DrkCde15 <jcesarsantana215@gmail.com>
2026-05-20 14:33:29 -0300

    feat: implementação de login

--- Flask/Pinterest-Clone/service/__init__.py
+++ Flask/Pinterest-Clone/service/__init__.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 base_dir = Path(__file__).resolve().parent.parent
 
 app = Flask(__name__, template_folder=base_dir / 'templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(base_dir / 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dr67ilpo8yfdsaw3456yhjlpoiuygfds'
+
 db = SQLAlchemy(app)
+bc = Bcrypt(app)
+lm = LoginManager(app)
+lm.login_view = 'index'
 
 from service import routes
--- Flask/Pinterest-Clone/service/routes.py
+++ Flask/Pinterest-Clone/service/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from service import app
+from flask_login import login_required, current_user
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/perfil/<user>')
+@login_required
 def perfil(user):
     return render_template('perfil.html', user=user)
