4ab890b2d75a3b69c4a07e6db969e7dc178f7edc
DrkCde15 <jcesarsantana215@gmail.com>
2026-05-21 10:16:17 -0300

    feat: adiciona formulários de login e criação de conta com validações

---
+++ Flask/Pinterest-Clone/service/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from service.models import User
+
+class FormLogin(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit_button = SubmitField('Login')
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('Create Account')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email já está em uso. Por favor, escolha outro email.')
+        
+    def validate_password(self, password):
+        if len(password.data) < 6:
+            raise ValidationError('A senha deve conter pelo menos 6 caracteres.')
+
+    def validate_confirm_password(self, confirm_password):
+        if confirm_password.data != self.password.data:
+            raise ValidationError('As senhas não coincidem.')
